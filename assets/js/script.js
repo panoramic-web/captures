5526edd50b57bf34bceba8b163e3348d909e7019
Panoramic web <92629860+panoramic-web@users.noreply.github.com>
2024-05-20 10:42:13 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
+var menuIcon = document.querySelector('#menu-icon');
+var closeIcon = document.querySelector('#close_icon');
+var linkWrapper = document.querySelector('.link_wrapper');
+var menuItems = document.querySelectorAll('.menu_list li a');
 
+menuIcon.onclick = () => {
+   linkWrapper.style.display = 'block';
+}
+
+closeIcon.onclick = () => {
+   linkWrapper.style.display = 'none';
+}
+
+menuItems.forEach(item => {
+   item.onclick = () => {
+      if (innerWidth <= 800) {
+         setTimeout(() => {
+            linkWrapper.style.display = 'none';
+         }, 300);
+      }
+   }
+})
